3c8912828e40bc4d928cbf51ec972958f1ab9e6f
gevorg-tsat <72250109+gevorg-tsat@users.noreply.github.com>
2023-08-02 12:32:19 +0300

    add docs generator

---
+++ src/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "3f",
+  1: "f",
+  2: "3"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Pages"
+};
+
